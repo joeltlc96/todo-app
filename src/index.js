d30f3d8c0f378a9b8f126dd684c53f90f9a57f3c
joel <joel.vinansaca@tecnologicoliceocristiano.edu.ec>
2025-05-08 21:16:48 -0500

    gf
    gf

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 import routerTODOS from "./routes/todo.routes.js";
+import cors from "cors";
+
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use(morgan("dev"));
+
 app.use(routerTODOS);
 
 app.listen(3000, () => console.log("Server is running on port ",3000));
